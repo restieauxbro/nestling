5a82f3b681332a8010f4baf78d5d67720334f731
restieauxbro <restieauxbro@hotmail.com>
2021-02-01 20:49:30 +1300

    seo data

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    title: `The Nestling Process`,
+    description: `A conversation framework for fostering a deep sense of safety, trust, and love in relationships.`,
     author: `@gatsbyjs`,
   },
   plugins: [
